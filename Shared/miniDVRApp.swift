9e293da09696cb0420a90b7f4fadef53b839cef5
Haren <human01d@icloud.com>
2021-07-21 07:06:28 +0800

    Initial Commit

---
+++ Shared/miniDVRApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniDVRApp.swift
+//  Shared
+//
+//  Created by 陈驰坤 on 2021/7/21.
+//
+
+import SwiftUI
+
+@main
+struct miniDVRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
